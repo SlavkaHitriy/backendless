f83b71a816ca78b74c23bd2c1c9494fae765312e
SlavkaHitriy <slavik.nukutuk@gmail.com>
2023-10-25 05:20:33 +0300

    Updates

---
+++ assets/dummyTable-34526f59.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-836e0cc7.js";const t=()=>e.jsx("table",{children:e.jsx("tbody",{children:e.jsxs("tr",{children:[e.jsx("td",{children:"Dummy"}),e.jsx("td",{children:"Table"})]})})});export{t as default};
