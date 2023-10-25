aa12ba98916c165cfac28b95a9b20e09095bbe9f
SlavkaHitriy <slavik.nukutuk@gmail.com>
2023-10-25 05:34:22 +0300

    Updates

--- assets/dummyList-fa5f3060.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-1f147dd9.js";const r=()=>s.jsxs("ul",{children:[s.jsx("li",{children:"Dummy"}),s.jsx("li",{children:"List"})]});export{r as default};
---
+++ assets/dummyTable-bd6d9c31.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-57581426.js";const t=()=>e.jsx("table",{children:e.jsx("tbody",{children:e.jsxs("tr",{children:[e.jsx("td",{children:"Dummy"}),e.jsx("td",{children:"Table"})]})})});export{t as default};
